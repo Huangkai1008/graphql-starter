a2533aa9d79a8d88471b4ac00c72046854a003e8
huangkai <18778335525@163.com>
2019-10-07 16:49:01 +0800

    graphql实例创建link

--- graphql_starter/schema/link.py
+++ graphql_starter/schema/link.py
@@ -17,4 +17,24 @@ class Query(graphene.ObjectType):
         return all_links
 
 
-schema = graphene.Schema(query=Query)
+class CreateLinkMutation(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    class Arguments:
+        url = graphene.String(required=True)
+        description = graphene.String(required=True)
+
+    async def mutate(self, info, url: str, description: str):
+        link = await Link.create(url=url, description=description)
+        return CreateLinkMutation(
+            id=link.id, url=link.url, description=link.description
+        )
+
+
+class Mutation(graphene.ObjectType):
+    create_link = CreateLinkMutation.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
